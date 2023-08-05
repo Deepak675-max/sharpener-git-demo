371bc264823f6325dbb9ff8972c1d9afa19d291e
deepak kumar <deepak.kumar@nourishgenie.in>
2023-08-05 23:09:43 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const items = document.querySelectorAll('.list-group-item');
-items.forEach(item => {
-    if (item.innerHTML == items[2].innerHTML) {
-        item.style.backgroundColor = "green";
-    }
-    item.style.fontWeight = 'bold';
-})
+// const items = document.querySelectorAll('.list-group-item');
+// items.forEach(item => {
+//     if (item.innerHTML == items[2].innerHTML) {
+//         item.style.backgroundColor = "green";
+//     }
+//     item.style.fontWeight = 'bold';
+// })
+
+const item = document.getElementsByClassName('last-item')
+
+item[0].style.color = "red";
+item[0].classList.add("list-group-item");
