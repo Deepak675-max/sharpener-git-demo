57228adc53ed24b3c770d09167d77b444026e350
deepak kumar <deepak.kumar@nourishgenie.in>
2023-08-05 22:51:35 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const header = document.getElementById('main-header');
-
-header.style.borderBottom = "solid 3px #000"
-
-const addItem = document.getElementById('title');
-
-addItem.style.color = "green";
-
-addItem.style.fontWeight = "bold";
-
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const inputValue = document.querySelector('#itemvalue');
-    const form = document.querySelector('#my-form');
-    var listItem = document.createElement("li");
-    listItem.classList.add("list-group-item")
-    listItem.appendChild(document.createTextNode(inputValue.value));
-    items.appendChild(listItem);
-    form.reset();
-    inputValue.focus()
+const items = document.querySelectorAll('.list-group-item');
+items.forEach(item => {
+    if (item.innerHTML == items[2].innerHTML) {
+        item.style.backgroundColor = "green";
+    }
+    item.style.fontWeight = 'bold';
 })
